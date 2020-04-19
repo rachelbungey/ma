696016b17e96686f086a2c01b5e82a7fd21ef6a0
Kirill Kovalevskiy <kovalewskiy@gmail.com>
2020-04-19 01:10:50 -0700

    Adjusted deploy

--- tools/deploy.sh
+++ tools/deploy.sh
@@ -3,6 +3,6 @@ yarn build
 touch dist/.nojekyll
 git add dist/
 git commit -m "New github pages deploy"
-git subtree split --prefix out -b gh-pages
+git subtree split --prefix dist -b gh-pages
 git push -f origin gh-pages:gh-pages
 git branch -D gh-pages
