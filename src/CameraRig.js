e2710484e1954060be1d0f5c4fc03ee96467cda3
Sneha Belkhale <sbelkhale@yahoo.com>
2020-04-19 21:00:52 -0700

    switch to default controllers

--- src/CameraRig.js
+++ src/CameraRig.js
@@ -8,8 +8,8 @@ const CameraRig = () => (
   <a-entity id="cameraRig" position="0 1 4">
     <a-camera id="camera" wasd-controls="acceleration:200" />
     <a-entity oculus-go-controls />
-    <a-entity custom-control="hand: left" quick-turn visible="false" />
-    <a-entity custom-control="hand: right" mover visible="false" />
+    <a-entity oculus-touch-controls="hand: left; orientationOffset: 0 0 0;" quick-turn visible="false" />
+    <a-entity oculus-touch-controls="hand: right; orientationOffset: 0 0 0;" mover visible="false" />
   </a-entity>
 );
 
